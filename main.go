dd4a345b8619f3793c3b6f531c8a5d041a157575
JulioAlb453 <JulioAlb231/jdevelop.u@gmail.com>
2025-02-28 10:20:27 -0600

    add server

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Get("/status", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, map[string]string{"status": "ok"})
+	})
+
+	r.Run(":8080")
+}
